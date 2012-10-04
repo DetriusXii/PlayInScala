80ab0cc5f858a6e8846862d89df1a25602bdde24
Andrijan Jelovic <ajelovic@yahoo.com>
2012-10-04 16:32:31 -0600

    I can render short province names.  Now I have to identify owned provinces back in the database

--- app/controllers/JdipSVGRenderer.scala
+++ app/controllers/JdipSVGRenderer.scala
@@ -4,6 +4,8 @@ import javax.xml.parsers._
 import java.io._
 import org.w3c.dom._
 import javax.xml.transform._
+import javax.xml.transform.dom._
+import javax.xml.transform.stream._
 
 object JdipSVGRenderer {
   val SVG_FILE_PATH = "public/images/egdipmap.svg"
@@ -17,20 +19,32 @@ object JdipSVGRenderer {
 
   implicit def getElementByIdOption(element: Element): Option[Element] =
     if (element != null) {
-      None
-    } else {
       Some(element)
+    } else {
+      None
     }
+
+  def 
 
   def getRenderedDocument = {
     val document = getUneditedDocument
 
     val elementOption: Option[Element] = document.getElementById(BRIEF_LABEL_LAYER_ID)
     elementOption.map(_.setAttribute(VISIBILITY_ATTRIBUTE, VISIBLE))
-    
-    document
-    
+    elementOption.map(_.setAttribute(
+   
+    println(elementOption)
+
     val transformer = TransformerFactory.newInstance.newTransformer
+    val domSource = new DOMSource(document.getDocumentElement)
+
+    val outputStream = new ByteArrayOutputStream()
+    val result = new StreamResult(outputStream)
+
+    transformer.transform(domSource, result)
+
+    val byteArrayInputStream = new ByteArrayInputStream(outputStream.toByteArray)
+    scala.xml.XML.load(byteArrayInputStream)
     
   }
 
