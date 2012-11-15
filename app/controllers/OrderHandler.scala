72ac9bb1fcbacff81c94d156d94f477d5c44b448
Andrijan Jelovic <ajelovic@yahoo.com>
2012-11-15 16:39:30 -0600

    Going to investigate Play's form structure further

--- app/controllers/OrderHandler.scala
+++ app/controllers/OrderHandler.scala
@@ -5,13 +5,13 @@ import scalaz._
 object OrderHandler extends Controller with OptionTs {
   val GAME_PLAYER_EMPIRE_ID_NAME = "gamePlayerEmpireID"
   val UNIT_NAME = "unit"
-  val ORDER_SUFFIX = "-order"
-  val SOURCE_SUFFIX = "-source"
-  val TARGET_SUFFIX = "-target"
+  val ORDER_SUFFIX = "order"
+  val SOURCE_SUFFIX = "source"
+  val TARGET_SUFFIX = "target"
   
   implicit def embedOptionInListT[A](option: Option[A]): ListT[Option, A] =
     option match {
-    	case Some(a: A) => a :: ListT.empty[Option, A]
+    	case Some(a) => a :: ListT.empty[Option, A]
     	case None => ListT.empty[Option, A]
   	}
   
@@ -20,16 +20,94 @@ object OrderHandler extends Controller with OptionTs {
     
   implicit def convertSeqToSingleString(seq: Seq[String]): String = seq.mkString("")
   
-  def submitMoveOrders: ApplicationAction[AnyContent] = new ApplicationAction {
-    Action { implicit request =>
+  def submitMoveOrders: ApplicationAction[AnyContent] = new ApplicationAction (
+    Action ( implicit request => {
       
-      for (postParameters <- request.body.asFormUrlEncoded;
-    	   gamePlayerEmpireID <- postParameters.get(GAME_PLAYER_EMPIRE_ID_NAME);
-    	   gamePlayerEmpire <- DBQueries.getGamePlayerEmpire(gamePlayerEmpireID);
-    	   dpu	<- DBQueries.getDiplomacyUnitsForGamePlayerEmpire(gamePlayerEmpire);
-    	   location <- DBQueries.locations.find(_.id == dpu.unitLocation);
-    	   order <- postParameters.get("%s-%s" format ())
-      ) 
-    }
+      val postParametersListT = embedOptionInListT(request.body.asFormUrlEncoded)
+      
+      val dpuAndLocsListT =
+        postParametersOptionT.flatMap(getDiplomacyUnitAndLocation)
+      
+      
+	})
+  )
+  
+  private def allUnitOrdersExist(
+      postParameters: Map[String, Seq[String]],
+      dpuAndLocsListT: ListT[Option, (DiplomacyUnit, Location)]): Boolean = {
+	  dpuAndLocsListT.foldLeft(true)((u, v) => {
+	    val unitLocation = v._2
+	    val formattedLocationName = 
+	      DiplomacyQueries.getFormattedLocationName(unitLocation)
+	    val orderForUnitOption = 
+	      postParameters.get("%s-%s" format (formattedLocationName, ORDER_SUFFIX))
+	    orderForUnitOption match {
+	      case Some(_) => u && true
+	      case None => u && false
+	    }
+	  })
+  }
+  
+  private def ordersMatchForUnitTypeAndPhase(
+		  postParameters: Map[String, Seq[String]],
+		  dpuAndLocsListT: ListT[Option, (DiplomacyUnit, Location)]): Boolean = {
+    dpuAndLocsListT.foldLeft(true)((u, v) => {
+      val dpu = v._1
+      val unitLocation = v._2
+      val formattedLocationName =
+        DiplomacyQueries.getFormattedLocationName(unitLocation)
+      val orderForUnitOption =
+        postParameters.get("%s-%s" format (formattedLocationName, ORDER_SUFFIX))
+      orderForUnitOption match {
+        case Some(x: Seq[String]) => x.mkString("") match {
+          case OrderType.HOLD | 
+          		OrderType.MOVE | 
+          		OrderType.SUPPORT_HOLD | 
+          		OrderType.SUPPORT_MOVE => v && true
+          case OrderType.CONVOY if dpu.UNIT_TYPE.equals(UnitType.FLEET) => u && true
+          case _ => u && false
+        }
+        case None => u && false
+      }
+    })
+  }
+  
+  private def analyzeOrders(postParameters: Map[String, Seq[String]],
+      dpuAndLocsListT: ListT[Option, (DiplomacyUnit, Location)]): Unit = {
+    val dpuAndFormattedLocsListT =
+      dpuAndLocsListT.map(u => (u._1, DiplomacyQueries.getFormattedLocationName(u._2)))
+    
+    
+    dpuAndFormattedLocsListT.map(u: (DiplomacyUnit, String) => {
+      val orderForUnitOption = postParameters.get("%s-%s" format(u._2, ORDER_SUFFIX))
+      
+      orderForUnitOption match {
+        case Some(x: Seq[String]) => x match {
+          case OrderType.HOLD => 
+          case OrderType.MOVE =>
+          case OrderType.SUPPORT_HOLD =>
+          case OrderType.SUPPORT_MOVE =>
+          case OrderType.CONVOY =>
+        }
+        case None => 
+      }
+    })
+  }
+  
+  private def getDiplomacyUnitAndLocation(
+      postParameters: Map[String, Seq[String]]): 
+      ListT[Option, Tuple2[DiplomacyUnit, Location]] = {
+    val gamePlayerEmpireIDOption = 
+      postParameters.get(GAME_PLAYER_EMPIRE_ID_NAME).map(_.mkString("")).map(_.toInt)
+    
+    val gamePlayerEmpireOption = 
+      gamePlayerEmpireIDOption.flatMap(DBQueries.getGamePlayerEmpire(_))
+    val diplomacyUnits = 
+      gamePlayerEmpireOption.flatMap(gpe =>
+        convertListToListT(DBQueries.getDiplomacyUnitsForGamePlayerEmpire(gpe)))
+    
+    for (dpu <- diplomacyUnits;
+    	loc <- DBQueries.locations.find(_.id == dpu.unitLocation)
+    ) yield ((dpu, location))
   }
 }
