9864ab7e7df3eb60e03197cdd253e22ef601cf91
Andrijan Jelovic <ajelovic@yahoo.com>
2013-01-28 07:18:31 -0600

    Working on OrderHandler still

--- app/controllers/OrderHandler.scala
+++ app/controllers/OrderHandler.scala
@@ -3,23 +3,9 @@ package controllers
 import play.api.mvc._
 import scalaz._
 import com.squeryl.jdip.tables._
-import com.squeryl.queries._
-import ReaderTExtension._
-import ReaderT._
+import com.squeryl.jdip.queries._
 
-object ReaderTExtension {
-  implicit def readerTFunctor[M[_]: Functor, R]: Functor[({type y[q]=ReaderT[M, R, q]})#y] = 
-    new Functor[({type y[q]= ReaderT[M, R, q]})#y] {
-	  def fmap[A, B](x: ReaderT[M, R, A], f: A => B) = new ReaderT[M, R, B] {
-		  val value = x.value ∘ (_ ∘ f)
-    }
-  }
-  
-  def readerTAsk[M[_], R](implicit m: Pure[M]) =
-	 ReaderT((r: R) => m.pure(r))
-}
-
-object OrderHandler extends Controller {
+object OrderHandler extends Controller with Kleislis {
   val GAME_PLAYER_EMPIRE_ID_NAME = "gamePlayerEmpireID"
   val UNIT_NAME = "unit"
   val ORDER_PREFIX = "order-"
@@ -41,30 +27,54 @@ object OrderHandler extends Controller {
   }
   
   
-  def handleOrder(, postParameters: Map[String, Seq[String]])
-  
   def processRequest(gpe: GamePlayerEmpire, 
-      postParameters: Map[String, Seq[String]]) = {
+		  username: String): Kleisli[Option, Map[String, Seq[String]], Unit] = {
     val dpuUnits = 
       DBQueries.getDiplomacyUnitsForGamePlayerEmpire(gpe)
     
-    dpuUnits.foreach(dpu => {
-      val unitOrderOption = 
-        postParameters.get("%s%s" format (ORDER_PREFIX, dpu.id))
-       
+    kleisli((r: Map[String, Seq[String]]) => {
+      dpuUnits.foreach(dpu => {
+        val unitOrderOption = r.get("%s%s" format (ORDER_PREFIX, dpu.id))
+        unitOrderOption.map(_ match {
+          case OrderType.HOLD => addHoldOrder(dpu)
+          case OrderType.MOVE | OrderType.SUPPORT_HOLD =>
+          case OrderType.SUPPORT_MOVE | OrderType.CONVOY =>
+        
+        })
+        
+        
+      })
+      
+      
     })
   }
+  
+  def addHoldOrder(dpu: DiplomacyUnit): 
   
     
   def submitMoveOrders: ApplicationAction[AnyContent] = new ApplicationAction (
     Action ( implicit request => {
-      implicit val postParametersOption = request.body.asFormUrlEncoded
-      val postEnvironment =
-        ReaderT.readerTPure[Option, Map[String, Seq[String]]].pure(Unit)
+      val postParametersOption = request.body.asFormUrlEncoded
+      postParametersOption.map((pp: Map[String, Seq[String]]) => {
+        val postEnvironment = ask[Option, Map[String, Seq[String]]]
+        
+        for (r <- postEnvironment;
+        	gpeID <- liftKleisli(r.get(GameScreenController.GAME_PLAYER_EMPIRE_ID));
+        	gamePlayerEmpire <- liftKleisli(DBQueries.getGamePlayerEmpire(gpeID.toInt));
+        	username <- liftKleisli(request.session.get(Security.username));
+        	_ <- kleisliPure(processRequest(gamePlayerEmpire, username)) if
+        		isGamePlayeEmpireInSession(gamePlayerEmpire, username)
+        )
+      })
       
-      fmap(postEnvironment, _ => {})
       
-      for (postParameters <- postParametersOption;
+      
+      for (postParameters <- )
+      
+      
+     // fmap(postEnvironment, _ => {})
+      
+      /*for (postParameters <- postParametersOption;
     	 ReaderT.readerTPure
     	 gpeID <- postParameters.
     	  	get(GameScreenController.GAME_PLAYER_EMPIRE_ID);
@@ -74,7 +84,7 @@ object OrderHandler extends Controller {
     	  	isGamePlayerEmpireInSession(gamePlayerEmpire, username)
       ) yield {
         isGamePlayerEmpireIDInSession
-      }
+      }*/
       
       Ok("Uoi have submitted your moves")
 	})
