0b8eaf08bb61397dbdc5e556cc0440df60ceca63
Andrijan Jelovic <ajelovic@yahoo.com>
2013-01-29 16:36:27 -0600

    OrderHandler.scala:  Finished the implementation of order handling. 	Working out some kinks with Kleisli

--- app/controllers/OrderHandler.scala
+++ app/controllers/OrderHandler.scala
@@ -16,11 +16,11 @@ object OrderHandler extends Controller with Kleislis {
     
   def isGamePlayerEmpireInSession(gpe: GamePlayerEmpire, 
       username: String): Boolean = {
-    for (player <- DBQueries.getPlayerForGamePlayerEmpire(gamePlayerEmpire)
+    (for (player <- DBQueries.getPlayerForGamePlayerEmpire(gpe)
     ) yield {
       player.id.equals(username)
-    } match {
-      case Some(x: Boolean) => x
+    }) match {
+      case Some(x) => x
       case None => false
     }
     
@@ -32,26 +32,86 @@ object OrderHandler extends Controller with Kleislis {
     val dpuUnits = 
       DBQueries.getDiplomacyUnitsForGamePlayerEmpire(gpe)
     
-    kleisli((r: Map[String, Seq[String]]) => {
-      dpuUnits.foreach(dpu => {
-        val unitOrderOption = r.get("%s%s" format (ORDER_PREFIX, dpu.id))
-        unitOrderOption.map(_ match {
+    dpuUnits.foldLeft(kleisliPure[Option, Map[String, Seq[String]]].pure(Unit))((kl, dpu) => {
+      kl.flatMap(r => {
+        val unitOrderOption = 
+          r.get("%s%d" format (ORDER_PREFIX, dpu.id)).map(_.toString)
+        unitOrderOption.map(unitOrder => unitOrder match {
           case OrderType.HOLD => addHoldOrder(dpu)
-          case OrderType.MOVE | OrderType.SUPPORT_HOLD =>
+          case OrderType.MOVE | OrderType.SUPPORT_HOLD => 
+            addBasicOrder(dpu, unitOrder)
           case OrderType.SUPPORT_MOVE | OrderType.CONVOY =>
-        
+            addAdvancedOrder(dpu, unitOrder)
         })
-        
-        
       })
-      
-      
     })
+      
   }
   
-  def addHoldOrder(dpu: DiplomacyUnit): 
-  
+  def addAdvancedOrder(dpu: DiplomacyUnit, unitOrder: String): 
+	  Kleisli[Option, Map[String, Seq[String]], Unit] = {
+    val postEnvironment = ask[Option, Map[String, Seq[String]]]
+    val srcOrderReader = postEnvironment.flatMap(r => 
+    	liftKleisli(r.get("%s%d" format (SOURCE_PREFIX, dpu.id)).
+    	    map(_.toString.toInt))).flatMap(srcLocationId => 
+    	    	liftKleisli(DBQueries.locations.find(_.id == srcLocationId))
+    	    )
+    val dstOrderReader = postEnvironment.flatMap(r =>
+    	liftKleisli(r.get("%s%d" format (TARGET_PREFIX, dpu.id)).
+    	    map(_.toString.toInt))).flatMap(dstLocationId => 
+    	    	liftKleisli(DBQueries.locations.find(_.id == dstLocationId))
+    	    )
     
+    for (srcLocation <- srcOrderReader;
+    	dstLocation <- dstOrderReader
+    ) yield {
+      val orderOption = DBQueries.getOrderForDiplomacyUnit(dpu)
+      orderOption match {
+        case Some(_) => UpdateStatements.updateMovementPhaseOrder(
+        		dpu, unitOrder, Some(srcLocation), Some(dstLocation))
+        case None => InsertStatements.updateMovementPhaseOrder(
+        		dpu, unitOrder, Some(srcLocation), Some(dstLocation)
+        )
+      }
+    }
+  }
+  
+  def addBasicOrder(dpu: DiplomacyUnit, unitOrder: String): 
+	  Kleisli[Option, Map[String, Seq[String]], Unit] = {
+    def postEnvironment = ask[Option, Map[String, Seq[String]]]
+    postEnvironment.flatMap(r => {
+      val simpleTargetIDOption = 
+        r.get("%s%d" format (SOURCE_PREFIX, dpu.id)).map(_.toString().toInt)
+      liftKleisli(simpleTargetID)
+    }).flatMap(simpleTargetID => {
+      val locOption = DBQueries.locations.find(_.id == simpleTargetID)
+      
+      val orderOption = DBQueries.getOrderForDiplomacyUnit(dpu)
+      
+      locOption.map(loc => orderOption match {
+        case Some(_) => 
+          UpdateStatements.updateMovementPhaseOrder(dpu, unitOrder,
+              Some(loc), None)
+        case None =>
+          InsertStatements.insertMovementPhaseOrder(dpu, unitOrder,
+              Some(loc), None)
+      })
+    })
+    
+  }
+  
+  def addHoldOrder(dpu: DiplomacyUnit): Kleisli[Option, 
+    Map[String, Seq[String]], Unit]  = 
+      kleisliPure[Option, Map[String, Seq[String]]].pure(DBQueries.getOrderForDiplomacyUnit(dpu) match {
+	      case Some(_) => 
+	        UpdateStatements.updateMovementPhaseOrder(dpu, 
+	            OrderType.HOLD, None, None)
+	      case None => 
+	        InsertStatements.insertMovementPhaseOrder(dpu, 
+	            OrderType.HOLD, None, None)
+	    })
+ 
+  
   def submitMoveOrders: ApplicationAction[AnyContent] = new ApplicationAction (
     Action ( implicit request => {
       val postParametersOption = request.body.asFormUrlEncoded
@@ -64,29 +124,11 @@ object OrderHandler extends Controller with Kleislis {
         	username <- liftKleisli(request.session.get(Security.username));
         	_ <- kleisliPure(processRequest(gamePlayerEmpire, username)) if
         		isGamePlayeEmpireInSession(gamePlayerEmpire, username)
-        )
+        ) yield { Unit }
       })
       
       
-      
-      for (postParameters <- )
-      
-      
-     // fmap(postEnvironment, _ => {})
-      
-      /*for (postParameters <- postParametersOption;
-    	 ReaderT.readerTPure
-    	 gpeID <- postParameters.
-    	  	get(GameScreenController.GAME_PLAYER_EMPIRE_ID);
-    	 gamePlayerEmpire <- DBQueries.getGamePlayerEmpire(gpeID.toInt);
-    	 username <- request.session.get(Security.username);
-    	  _ <- processRequest(gamePlayerEmpire, postParameters) if 
-    	  	isGamePlayerEmpireInSession(gamePlayerEmpire, username)
-      ) yield {
-        isGamePlayerEmpireIDInSession
-      }*/
-      
-      Ok("Uoi have submitted your moves")
+      Ok("You have submitted your moves")
 	})
   )
  
